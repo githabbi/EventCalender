10808740758ac73f7dc4ea0d5ecc176a75ae2ff7
githabbi <habib.chelbi@supcom.tn>
2024-04-04 02:17:43 +0100

    update1

--- calendar/routes/web.php
+++ calendar/routes/web.php
@@ -2,11 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+
+
+Route::get('/calendar', function () {
+    return view('calendar');
+})->name('calendar');
+Route::get('/login', function () {
+    return view('Login');
+})->name('login');
+
 Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('/create-event', function () {
-    return view('create-event');
-});
+    return view('Welcome');
+})->name('welcome');
