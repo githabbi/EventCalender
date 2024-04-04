ed95197707402b1e35651357e85eb12d5bc47cd6
rana chouchen <ranachouchen4@gmail.com>
2024-04-04 04:18:48 +0100

    gg

---
+++ calendar/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+    
+use App\Http\Controllers\PHPMailercontroller;
+    
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/',function(){
+   return view('calendrierr');
+});   
+Route::get('/send',[PHPMailercontroller::class, 'index'])->name('send.php.mailer');
+Route::post('/sent',[PHPMailercontroller::class, 'store'])->name('send.php.mailer.submit');
+Route::get('/dbconn',function(){
+   return view('dbconn');
+});
