1e6601517ac3b6ead8ad23e5b2538d66de1bcde4
githabbi <habib.chelbi@supcom.tn>
2024-04-13 03:32:31 +0100

    test

--- calendar/public/assets/js/main.js
+++ calendar/public/assets/js/main.js
@@ -1,29 +1,5 @@
-// add clicked class to selected list item on click
-let list = document.querySelectorAll(".navigation li");
-
-function activeLink() {
-  // Remove "clicked" class from all items
-  list.forEach((item) => {
-    item.classList.remove("clicked");
-  });
-  // Add "clicked" class to the clicked item
-  this.classList.add("clicked");
-}
-
-list.forEach((item) => item.addEventListener("click", activeLink));
-
-// Menu Toggle
-let toggle = document.querySelector(".toggle");
-let navigation = document.querySelector(".navigation");
-let main = document.querySelector(".main");
-
-toggle.onclick = function () {
-  navigation.classList.toggle("active");
-  main.classList.toggle("active");
-};
-
 // add hovered class to selected list item
-/*let list = document.querySelectorAll(".navigation li");
+let list = document.querySelectorAll(".navigation li");
 
 function activeLink() {
   list.forEach((item) => {
@@ -42,4 +18,4 @@ let main = document.querySelector(".main");
 toggle.onclick = function () {
   navigation.classList.toggle("active");
   main.classList.toggle("active");
-};*/
+};
